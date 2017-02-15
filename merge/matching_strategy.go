0188e88d8ea4fa7052a97b7a3b2c49995c57627c
Troy Crews <troycrews@gmail.com>
2017-02-15 12:56:22 -0500

    Added fixtures and nil checks for all patient types

--- merge/matching_strategy.go
+++ merge/matching_strategy.go
@@ -40,142 +40,170 @@ func (p *PatientMatchingStrategy) Match(left interface{}, right interface{}) (is
 	const matchCriteriaTotal = 6.0
 
 	// Property comparison
-	if leftResource.Name[0].Given[0] == rightResource.Name[0].Given[0] && leftResource.Name[0].Family[0] == rightResource.Name[0].Family[0] {
-		matchCtr++
-		fmt.Println("Name: match")
-	}
-
-	if leftResource.Active == rightResource.Active {
-		matchCtr++
-		fmt.Println("Active: match")
-	}
-
-	if leftResource.Gender == rightResource.Gender {
-		matchCtr++
-		fmt.Println("Gender: match")
-	}
-
-	if leftResource.BirthDate.Time == rightResource.BirthDate.Time {
-		matchCtr++
-		fmt.Println("Birthdate: match")
-	}
-
-	if leftResource.DeceasedBoolean == rightResource.DeceasedBoolean {
-		matchCtr++
-		fmt.Println("DeceasedBoolean: match")
-	}
-
-	if leftResource.MaritalStatus.Text == rightResource.MaritalStatus.Text {
-		matchCtr++
-		fmt.Println("MaritalStatus: match")
-	}
-
-	if len(leftResource.Telecom) == len(rightResource.Telecom) {
-		for i := 0; i < len(leftResource.Telecom); i++ {
-			if leftResource.Telecom[i] == rightResource.Telecom[i] {
-				matchCtr++
-				fmt.Println("Telecom: match")
-			}
-		}
-	}
-	if len(leftResource.Address) == len(rightResource.Address) {
-		addMatchCtr := 0.0
-		addMatchCriteriaTotal := 8.0
-		for i := 0; i < len(leftResource.Address); i++ {
-			if leftResource.Address[i].Use == rightResource.Address[i].Use {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].Type == rightResource.Address[i].Type {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].Text == rightResource.Address[i].Text {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].City == rightResource.Address[i].City {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].District == rightResource.Address[i].District {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].State == rightResource.Address[i].State {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].PostalCode == rightResource.Address[i].PostalCode {
-				addMatchCtr++
-			}
-
-			if leftResource.Address[i].Country == rightResource.Address[i].Country {
-				addMatchCtr++
-			}
-		}
-		if IsMatchFromCriteria(matchThreshold, addMatchCtr, addMatchCriteriaTotal) {
+	if len(leftResource.Name) > 0 && len(rightResource.Name) > 0 {
+		if leftResource.Name[0].Given[0] == rightResource.Name[0].Given[0] && leftResource.Name[0].Family[0] == rightResource.Name[0].Family[0] {
 			matchCtr++
 		}
 	}
-	if leftResource.MultipleBirthBoolean == rightResource.MultipleBirthBoolean {
-		matchCtr++
-	}
-	if leftResource.MultipleBirthInteger == rightResource.MultipleBirthInteger {
-		matchCtr++
-	}
-	if leftResource.Animal == rightResource.Animal {
-		matchCtr++
-	}
-	if len(leftResource.Communication) == len(rightResource.Communication) {
-		commMatchCtr := 0.0
-		commMatchCriteriaTotal := 2.0
-		for i := 0; i < len(leftResource.Communication); i++ {
-			if leftResource.Communication[i].Language == rightResource.Communication[i].Language {
-				commMatchCtr++
-			}
-			if leftResource.Communication[i].Preferred == rightResource.Communication[i].Preferred {
-				commMatchCtr++
-			}
-		}
-		if IsMatchFromCriteria(matchThreshold, commMatchCtr, commMatchCriteriaTotal) {
+
+	if leftResource.Active != nil && rightResource.Active != nil {
+		if leftResource.Active == rightResource.Active {
 			matchCtr++
 		}
 	}
-	if len(leftResource.Contact) == len(rightResource.Contact) {
-		contactMatchCtr := 0.0
-		contactCriteriaTotal := 6.0
 
-		for i := 0; i < len(leftResource.Contact); i++ {
-			if leftResource.Contact[i].Relationship[0].Text == rightResource.Contact[i].Relationship[0].Text {
-				contactMatchCtr++
-			}
-			if leftResource.Contact[i].Name == rightResource.Contact[i].Name {
-				contactMatchCtr++
-			}
-			if len(leftResource.Telecom) == len(rightResource.Telecom) {
-				for i := 0; i < len(leftResource.Telecom); i++ {
-					if leftResource.Telecom[i] == rightResource.Telecom[i] {
-						contactMatchCtr++
-					}
+	if leftResource.Gender != "" && rightResource.Gender != "" {
+		if leftResource.Gender == rightResource.Gender {
+			matchCtr++
+		}
+	}
+
+	if leftResource.BirthDate != nil && rightResource.BirthDate != nil {
+		if leftResource.BirthDate.Time == rightResource.BirthDate.Time {
+			matchCtr++
+		}
+	}
+
+	if leftResource.DeceasedBoolean != nil && rightResource.DeceasedBoolean != nil {
+		if leftResource.DeceasedBoolean == rightResource.DeceasedBoolean {
+			matchCtr++
+		}
+	}
+
+	if leftResource.MaritalStatus != nil && rightResource.MaritalStatus != nil {
+		if leftResource.MaritalStatus.Text == rightResource.MaritalStatus.Text {
+			matchCtr++
+		}
+	}
+
+	if len(leftResource.Telecom) > 0 && len(rightResource.Telecom) > 0 {
+		if len(leftResource.Telecom) == len(rightResource.Telecom) {
+			for i := 0; i < len(leftResource.Telecom); i++ {
+				if leftResource.Telecom[i] == rightResource.Telecom[i] {
+					matchCtr++
 				}
 			}
-			if leftResource.Contact[i].Address == rightResource.Contact[i].Address {
-				contactMatchCtr++
+		}
+	}
+
+	if len(leftResource.Address) > 0 && len(rightResource.Address) > 0 {
+		if len(leftResource.Address) == len(rightResource.Address) {
+			addMatchCtr := 0.0
+			addMatchCriteriaTotal := 8.0
+			for i := 0; i < len(leftResource.Address); i++ {
+				if leftResource.Address[i].Use == rightResource.Address[i].Use {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].Type == rightResource.Address[i].Type {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].Text == rightResource.Address[i].Text {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].City == rightResource.Address[i].City {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].District == rightResource.Address[i].District {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].State == rightResource.Address[i].State {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].PostalCode == rightResource.Address[i].PostalCode {
+					addMatchCtr++
+				}
+
+				if leftResource.Address[i].Country == rightResource.Address[i].Country {
+					addMatchCtr++
+				}
 			}
-			if leftResource.Contact[i].Gender == rightResource.Contact[i].Gender {
-				contactMatchCtr++
-			}
-			if leftResource.Contact[i].Organization == rightResource.Contact[i].Organization {
-				contactMatchCtr++
+			if IsMatchFromCriteria(matchThreshold, addMatchCtr, addMatchCriteriaTotal) {
+				matchCtr++
 			}
 		}
-		if IsMatchFromCriteria(matchThreshold, contactMatchCtr, contactCriteriaTotal) {
+	}
+
+	if leftResource.MultipleBirthBoolean != nil && rightResource.MultipleBirthBoolean != nil {
+		if leftResource.MultipleBirthBoolean == rightResource.MultipleBirthBoolean {
 			matchCtr++
 		}
 	}
-	if leftResource.ManagingOrganization == rightResource.ManagingOrganization {
-		matchCtr++
+
+	if leftResource.MultipleBirthInteger != nil && rightResource.MultipleBirthInteger != nil {
+		if leftResource.MultipleBirthInteger == rightResource.MultipleBirthInteger {
+			matchCtr++
+		}
+	}
+
+	if leftResource.Animal != nil && rightResource.Animal != nil {
+		if leftResource.Animal == rightResource.Animal {
+			matchCtr++
+		}
+	}
+
+	if len(leftResource.Communication) > 0 && len(rightResource.Communication) > 0 {
+		if len(leftResource.Communication) == len(rightResource.Communication) {
+			commMatchCtr := 0.0
+			commMatchCriteriaTotal := 2.0
+			for i := 0; i < len(leftResource.Communication); i++ {
+				if leftResource.Communication[i].Language == rightResource.Communication[i].Language {
+					commMatchCtr++
+				}
+				if leftResource.Communication[i].Preferred == rightResource.Communication[i].Preferred {
+					commMatchCtr++
+				}
+			}
+			if IsMatchFromCriteria(matchThreshold, commMatchCtr, commMatchCriteriaTotal) {
+				matchCtr++
+			}
+		}
+	}
+
+	if len(leftResource.Contact) > 0 && len(rightResource.Contact) > 0 {
+		if len(leftResource.Contact) == len(rightResource.Contact) {
+			contactMatchCtr := 0.0
+			contactCriteriaTotal := 6.0
+
+			for i := 0; i < len(leftResource.Contact); i++ {
+				if leftResource.Contact[i].Relationship[0].Text == rightResource.Contact[i].Relationship[0].Text {
+					contactMatchCtr++
+				}
+				if leftResource.Contact[i].Name == rightResource.Contact[i].Name {
+					contactMatchCtr++
+				}
+				if len(leftResource.Telecom) == len(rightResource.Telecom) {
+					for i := 0; i < len(leftResource.Telecom); i++ {
+						if leftResource.Telecom[i] == rightResource.Telecom[i] {
+							contactMatchCtr++
+						}
+					}
+				}
+				if leftResource.Contact[i].Address == rightResource.Contact[i].Address {
+					contactMatchCtr++
+				}
+				if leftResource.Contact[i].Gender == rightResource.Contact[i].Gender {
+					contactMatchCtr++
+				}
+				if leftResource.Contact[i].Organization == rightResource.Contact[i].Organization {
+					contactMatchCtr++
+				}
+			}
+			if IsMatchFromCriteria(matchThreshold, contactMatchCtr, contactCriteriaTotal) {
+				matchCtr++
+			}
+		}
+	}
+
+	if leftResource.ManagingOrganization != nil && rightResource.ManagingOrganization != nil {
+		if leftResource.ManagingOrganization == rightResource.ManagingOrganization {
+			matchCtr++
+		}
 	}
 
 	isMatch = IsMatchFromCriteria(matchThreshold, matchCtr, matchCriteriaTotal)
@@ -184,5 +212,6 @@ func (p *PatientMatchingStrategy) Match(left interface{}, right interface{}) (is
 
 func IsMatchFromCriteria(threshold, score, totalCriteria float64) bool {
 	resultAvg := score / totalCriteria
+	fmt.Println(resultAvg)
 	return resultAvg >= threshold
 }
