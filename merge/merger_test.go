fb068aeeb0feae8392e943222a3b9a989769c048
Carlton Duffett <cjduffett@comcast.net>
2017-02-25 15:37:56 -0500

    Updated merge implementation
    Still need to write the tests.

--- merge/merger_test.go
+++ merge/merger_test.go
@@ -1,15 +1,10 @@
 package merge
 
 import (
-	"fmt"
-	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"gopkg.in/mgo.v2/bson"
-
 	"github.com/gin-gonic/gin"
-	"github.com/intervention-engine/fhir/models"
 	"github.com/intervention-engine/fhir/server"
 	"github.com/mitre/ptmerge/testutil"
 	"github.com/stretchr/testify/suite"
@@ -53,13 +48,7 @@ func (m *MergerTestSuite) TearDownTest() {
 // ========================================================================= //
 
 func (m *MergerTestSuite) TestMerge() {
-	merger := NewMerger(m.FHIRServer.URL)
-	source1 := m.FHIRServer.URL + "/Bundle/" + bson.NewObjectId().Hex()
-	source2 := m.FHIRServer.URL + "/Bundle/" + bson.NewObjectId().Hex()
-	outcome, targetURL, err := merger.Merge(source1, source2)
-	m.Nil(outcome)
-	m.Equal("", targetURL)
-	m.Nil(err)
+
 }
 
 // ========================================================================= //
@@ -67,101 +56,5 @@ func (m *MergerTestSuite) TestMerge() {
 // ========================================================================= //
 
 func (m *MergerTestSuite) TestResolveConflict() {
-	merger := NewMerger(m.FHIRServer.URL)
-	targetURL := m.FHIRServer.URL + "/Bundle/" + bson.NewObjectId().Hex()
-	opOutcomeURL := m.FHIRServer.URL + "/OperationOutcome/" + bson.NewObjectId().Hex()
-	outcome, err := merger.ResolveConflict(targetURL, opOutcomeURL, nil)
-	m.Nil(outcome)
-	m.Nil(err)
-}
 
-// ========================================================================= //
-// TEST THE HELPER FUNCTIONS                                                 //
-// ========================================================================= //
-
-func (m *MergerTestSuite) TestGetOperationOutcome() {
-	// Testing with an OperationOutcome.
-	oo, err := testutil.PostOperationOutcome(m.FHIRServer.URL, "../fixtures/one_conflict/conflict_oo.json")
-	m.NoError(err)
-
-	uri := m.FHIRServer.URL + "/OperationOutcome/" + oo.Resource.Id
-	resource, err := GetResource("OperationOutcome", uri)
-	m.NoError(err)
-
-	// Check that the resource returned is what we expect.
-	resOpOutcome, ok := resource.(*models.OperationOutcome)
-	m.True(ok)
-	m.Equal(oo.Resource.Id, resOpOutcome.Resource.Id)
-}
-
-func (m *MergerTestSuite) TestGetBundleResource() {
-	// Also testing with a Bundle.
-	bundle, err := testutil.PostPatientBundle(m.FHIRServer.URL, "../fixtures/john_peters_bundle.json")
-	m.NoError(err)
-
-	uri := m.FHIRServer.URL + "/Bundle/" + bundle.Resource.Id
-	resource, err := GetResource("Bundle", uri)
-	m.NoError(err)
-
-	// Check that the resource returned is what we expect.
-	resBundle, ok := resource.(*models.Bundle)
-	m.True(ok)
-	m.Len(resBundle.Entry, 19)
-}
-
-func (m *MergerTestSuite) TestGetResourceNotFound() {
-	// Attempting to get a resource that doesn't exist should throw an error.
-	resourceID := bson.NewObjectId().Hex()
-	uri := m.FHIRServer.URL + "/Patient/" + resourceID
-	_, err := GetResource("Patient", uri)
-	m.Error(err)
-	m.Equal(fmt.Sprintf("Resource %s not found", uri), err.Error())
-}
-
-func (m *MergerTestSuite) TestGetResourceMismatchedType() {
-	bundle, err := testutil.PostPatientBundle(m.FHIRServer.URL, "../fixtures/john_peters_bundle.json")
-	m.NoError(err)
-
-	uri := m.FHIRServer.URL + "/Bundle/" + bundle.Resource.Id
-	_, err = GetResource("Patient", uri) // Patient type does not match the resource
-	m.Error(err)
-	m.Equal("Expected resourceType to be Patient, instead received Bundle", err.Error())
-}
-
-func (m *MergerTestSuite) TestDeleteOperationOutcomeResource() {
-	// Testing with an OperationOutcome.
-	oo, err := testutil.PostOperationOutcome(m.FHIRServer.URL, "../fixtures/one_conflict/conflict_oo.json")
-	m.NoError(err)
-
-	uri := m.FHIRServer.URL + "/OperationOutcome/" + oo.Resource.Id
-	err = DeleteResource(uri)
-	m.NoError(err)
-
-	// Attempt to get the resource to confirm it's deleted.
-	res, err := http.Get(uri)
-	m.NoError(err)
-	m.Equal(404, res.StatusCode)
-}
-
-func (m *MergerTestSuite) TestDeleteBundleResource() {
-	// Also testing with a Bundle.
-	bundle, err := testutil.PostPatientBundle(m.FHIRServer.URL, "../fixtures/john_peters_bundle.json")
-	m.NoError(err)
-
-	uri := m.FHIRServer.URL + "/Bundle/" + bundle.Resource.Id
-	err = DeleteResource(uri)
-	m.NoError(err)
-
-	// Attempt to get the resource to confirm it's deleted.
-	res, err := http.Get(uri)
-	m.NoError(err)
-	m.Equal(404, res.StatusCode)
-}
-
-func (m *MergerTestSuite) TestDeleteResourceNotFound() {
-	// Attempting to delete a resource that already doesn't exist will not throw an error.
-	resourceID := bson.NewObjectId().Hex()
-	uri := m.FHIRServer.URL + "/Patient/" + resourceID
-	err := DeleteResource(uri)
-	m.NoError(err)
 }
